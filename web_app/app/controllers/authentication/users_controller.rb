66c9ebca971b6cd60e85aceeca221bedad64a754
Michał Dziardziel <mdziardziel@gmail.com>
2024-06-18 18:42:35 +0200

    use form for update user helps in adding validations for update password action

--- web_app/app/controllers/authentication/users_controller.rb
+++ web_app/app/controllers/authentication/users_controller.rb
@@ -1,14 +1,25 @@
 class Authentication::UsersController < Authentication::ApplicationController
+  before_action :require_login
+
+  def show
+    @user = Authentication::User.find(current_user.id)
+  end
+
   def edit
-    @user = Authentication::User.find(params[:id])
+    @user_form = Authentication::UpdateUserForm.new(
+      Authentication::User.find(current_user.id)
+    )
   end
 
   def update
-    @user = Authentication::User.find(params[:id])
+    @user_form = Authentication::UpdateUserForm.new(
+      Authentication::User.find(current_user.id),
+      user_params
+    )
 
-    if @user.update(user_params)
+    if @user_form.submit
       flash[:notice] = "User updated successfully"
-      redirect_to authentication_user_path(@user.id), status: :ok
+      redirect_to authentication_user_path
     else
       flash[:alert] = "User not updated"
       render :edit, status: :unprocessable_entity
@@ -18,6 +29,7 @@ class Authentication::UsersController < Authentication::ApplicationController
   private
 
   def user_params
-    params.require(:authentication_user).permit(:password, :password_confirmation)
+    params.require(:authentication_user)
+      .permit(:current_password, :new_password, :new_password_confirmation)
   end
 end
--- web_app/test/controllers/authentication/users_controller_test.rb
+++ web_app/test/controllers/authentication/users_controller_test.rb
@@ -1,4 +1,65 @@
-require "test_helper"
+require 'test_helper'
 
 class Authentication::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = authentication_users(:one)
+    @update_params = {
+      authentication_user: {
+        current_password: 'MyString',
+        new_password: 'NewPassword',
+        new_password_confirmation: 'NewPassword'
+      }
+    }
+  end
+
+  test "should get show when logged in" do
+    login_as(@user)
+    get authentication_user_url
+    assert_response :success
+    assert_select "span", "MyString"
+  end
+
+  test "shouldn't get show when not logged in" do
+    get authentication_user_url
+    assert_redirected_to new_authentication_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    login_as(@user)
+    get authentication_user_edit_url
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "shouldn't get edit when not logged in" do
+    get authentication_user_edit_url
+    assert_redirected_to new_authentication_user_session_path
+  end
+
+  test "should update user with valid params" do
+    login_as(@user)
+    patch authentication_user_url, params: @update_params
+    assert_redirected_to authentication_user_path
+    assert_equal "User updated successfully", flash[:notice]
+    assert @user.reload.authenticate(@update_params.dig(:authentication_user, :new_password))
+  end
+
+  test "shouldn't update user with valid params when not logged in" do
+    patch authentication_user_url, params: @update_params
+    assert_redirected_to new_authentication_user_session_path
+  end
+
+  test "should not update user with invalid params" do
+    login_as(@user)
+    @update_params[:authentication_user][:new_password_confirmation] = 'Mismatch'
+    patch authentication_user_url, params: @update_params
+    assert_response :unprocessable_entity
+    assert_equal "User not updated", flash[:alert]
+  end
+
+  private
+
+  def login_as(user)
+    post authentication_user_sessions_url, params: { authentication_user: { email: user.email, password: 'MyString' } }
+  end
 end
