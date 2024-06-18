cc62e3094a2ce98369d683d4376968de6f186393
Michał Dziardziel <mdziardziel@gmail.com>
2024-06-18 18:48:22 +0200

    update tests

--- web_app/test/models/authentication/user_test.rb
+++ web_app/test/models/authentication/user_test.rb
@@ -1,7 +1,43 @@
-require "test_helper"
+require 'test_helper'
 
 class Authentication::UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = authentication_users(:one)
+    @community = authentication_communities(:one)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @user.valid?
+  end
+
+  test "should not be valid without email" do
+    @user.email = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:email], "can't be blank"
+  end
+
+  test "should not be valid with duplicate email" do
+    duplicate_user = Authentication::User.new(email: @user.email, password: 'password', community: @community)
+    assert_not duplicate_user.valid?
+    assert_includes duplicate_user.errors[:email], "has already been taken"
+  end
+
+  test "should not be valid without password" do
+    @user.password = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:password], "can't be blank"
+  end
+
+  test "should belong to a community" do
+    assert_respond_to @user, :community
+    assert_equal @community, @user.community
+  end
+
+  test "should authenticate with correct password" do
+    assert @user.authenticate('MyString')
+  end
+
+  test "should not authenticate with incorrect password" do
+    assert_not @user.authenticate('wrongpassword')
+  end
 end
